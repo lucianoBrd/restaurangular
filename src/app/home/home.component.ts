7e353ae0ea863b11540d04206768c738484a1d37
lucianoBrd <lucien.burdet@gmail.com>
2019-11-15 08:44:45 +0100

    add a card

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  burger = 'assets/images/burger.jpg';
+
   constructor() { }
 
   ngOnInit() {
