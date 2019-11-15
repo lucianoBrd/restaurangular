e81de3aa650dde5bba5217b9be2e0e816fb54504
lucianoBrd <lucien.burdet@gmail.com>
2019-11-15 14:50:50 +0100

    form in two times

--- src/app/reserve/reserve.component.ts
+++ src/app/reserve/reserve.component.ts
@@ -13,6 +13,8 @@ export class ReserveComponent implements OnInit {
   timeOptions = ["12:00", "12:30", "13:00", "13:30", "19:00", "19:30", "20:00", "20:30"];
   peopleOptions = [1, 2, 3, 4, 5, 6, 7, 8];
 
+  isScheduleOk = false;
+
   client = {firstName: '', lastName: '', email: '', phone: ''};
 
   constructor(private fb: FormBuilder) { }
@@ -27,6 +29,13 @@ export class ReserveComponent implements OnInit {
       time: "",
       people: 1
     });
+  }
+
+  saveReservation(){
+    if(this.reservationForm.valid){
+      console.log(this.reservationForm);
+      this.isScheduleOk = true;
+    }
   }
 
   finalizeReservation(form){
