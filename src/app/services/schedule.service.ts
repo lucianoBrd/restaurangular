5109b4d4aa83c809d13f038f5cebf9e6b97d4845
lucianoBrd <lucien.burdet@gmail.com>
2019-11-17 21:14:56 +0100

    apply filter

--- src/app/services/schedule.service.ts
+++ src/app/services/schedule.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,10 @@ export class ScheduleService {
   constructor(private httpClient: HttpClient) { }
 
   search(term): Observable<Object> {
-    return this.httpClient.get('assets/schedules.json');
+    return this.httpClient
+      .get('assets/schedules.json')
+      .pipe(
+        map(res => res['events'].filter(evt => evt.title.indexOf(term) > -1))
+      );
   }
 }
